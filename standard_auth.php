111f87576331ac5a56ca640b6e9a99b1d2bed821
Anand <anandtrex@users.noreply.github.com>
2011-11-28 15:15:28 -0600

    Added eid authentication

---
+++ standard_auth.php
@@ -0,0 +1,30 @@
+<?
+    class Standard_Auth
+    {
+        function Standard_Auth()
+        {
+            
+        }
+        
+        function isLoggedIn()
+        {
+            return true;
+        }
+
+        function login($site_url)
+        {
+            
+        }
+
+        function getUserName()
+        {
+            return "test";
+        }
+
+        function logout()
+        {
+            
+        }
+
+    }
+?>
