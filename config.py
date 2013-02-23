900bd982ac0a247faae28516402aba23e42055df
Anand <anandtrex@users.noreply.github.com>
2013-02-22 23:34:49 -0800

    Changes for testing

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 
-APP_IP_ADDRESS = 'collabdraw.heroku.com'
-APP_PORT = os.environ['PORT'] if os.environ['PORT'] else 5000
+APP_IP_ADDRESS = '192.168.1.134' #'collabdraw.heroku.com'
+APP_PORT = os.environ.get('PORT', 5000)
 
-REDIS_IP_ADDRESS = 'pub-redis-15544.us-east-1-4.3.ec2.garantiadata.com'
-REDIS_PORT = 15544 
-
-ROOT_DIR = '/app'
+REDIS_IP_ADDRESS = 'localhost' #'pub-redis-15544.us-east-1-4.3.ec2.garantiadata.com'
+REDIS_PORT = 6379 #15544 
+ROOT_DIR = '.' #'/app'
 RESOURCE_DIR = ROOT_DIR + '/resource'
