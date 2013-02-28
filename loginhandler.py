e9f56b0cfb55e1d88fe2a5b2c6d959187d607156
Anand <anandtrex@users.noreply.github.com>
2013-02-27 21:02:13 -0800

    Quick bug fix

--- loginhandler.py
+++ loginhandler.py
@@ -17,12 +17,13 @@ class LoginHandler(tornado.web.RequestHandler):
       login_password = self.get_argument("loginPassword")
       redis_key = "users:%s" % login_id
       db_password = self.redis_client.get(redis_key)
-      db_password = db_password.decode('utf-8')
-      if db_password != hash_password(login_password):
-        self.logger.debug("db_password was %s but login_password was %s" % (db_password,
-            login_password))
-        self.finish('{"result": "failure"}')
-        return
-      self.logger.info("Logging in user %s", login_id)
-      self.set_secure_cookie("loginId", login_id)
-      self.finish('{"result": "success"}')
+      if db_password:
+        db_password = db_password.decode('utf-8')
+        if db_password != hash_password(login_password):
+            self.logger.debug("db_password was %s but login_password was %s" % (db_password,
+                login_password))
+            self.finish('{"result": "failure"}')
+            return
+        self.logger.info("Logging in user %s", login_id)
+        self.set_secure_cookie("loginId", login_id)
+        self.finish('{"result": "success"}')
