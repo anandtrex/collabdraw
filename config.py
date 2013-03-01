108e697d6e444df8c9336a519e646712fd3c52bb
Anand <anandtrex@users.noreply.github.com>
2013-03-01 07:03:29 +0000

    Renaming to BollacBoard

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
 # App's host and port
-APP_IP_ADDRESS = "192.168.1.134" #"collab.cloudapp.net"
+APP_IP_ADDRESS = "collab.cloudapp.net"
 APP_PORT = os.environ.get('PORT', 5000)
 
 # Port in which websocket client should listen
 # Usually same ass APP_PORT unless some other
 # port forwarding is set up
-PUBLIC_LISTEN_PORT = APP_PORT
+PUBLIC_LISTEN_PORT = 80
 
 # Redis host and port
 REDIS_IP_ADDRESS = 'localhost'
 REDIS_PORT = 6379
 
-# Full path of "collabdraw" directory
+# Full path of "bollacboard" directory
 ROOT_DIR = "/".join(os.path.realpath(__file__).split('/')[:-1])
 RESOURCE_DIR = ROOT_DIR + '/resource'
 
 # Hash salt for storing password in database
-HASH_SALT = "collabdraw"
+HASH_SALT = "bollacboard"
 
 # Enable SSL/TLS
 ENABLE_SSL = False
