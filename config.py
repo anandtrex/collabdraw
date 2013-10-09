659727741a5bd60491f8c9c3601b7530f7a676de
Anand <anandtrex@users.noreply.github.com>
2013-10-09 00:14:31 -0700

    Updating public listen port

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ APP_IP_ADDRESS = "collabdraw.herokuapp.com"
 APP_PORT = os.environ.get('PORT', 5000)
 
 # Port in which websocket client should listen
-# Usually same as APP_PORT unless some other
-# port forwarding is set up (for ex. if you're using heroku)
-PUBLIC_LISTEN_PORT = APP_PORT
+# Usually same ass APP_PORT unless some other
+# port forwarding is set up
+PUBLIC_LISTEN_PORT = 80
 
 PUBSUB_CLIENT_TYPE = 'redis' # only redis supported now
 DB_CLIENT_TYPE = 'redis'  # only redis supported now
